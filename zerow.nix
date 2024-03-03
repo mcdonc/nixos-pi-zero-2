ead8d5234005d7f22613c343bc27908ea35b58ca
Mr Z <denver.cfman@gmail.com>
2024-03-03 15:11:20 -0700

    Update zerow.nix

--- zerow.nix
+++ zerow.nix
@@ -12,7 +12,7 @@
 
   sdImage = {
     compressImage = false;
-    imageName = "pi.img";
+    imageName = "pi0.img";
 
     extraFirmwareConfig = {
       # Give up VRAM for more Free System Memory
@@ -38,6 +38,6 @@
     ];
   };
 
-  networking.hostName = "nixos-zero2w";
+  networking.hostName = "nixos-0w";
 
 }
