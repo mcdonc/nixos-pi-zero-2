ed0ef0bc9f920c5817f690b6a94ef8bc2f5464e6
Mr Z <denver.cfman@gmail.com>
2024-03-05 21:06:36 -0700

    Update common.nix

--- common.nix
+++ common.nix
@@ -94,7 +94,7 @@ in
   services.timesyncd.enable = true;
 
   # ! Change the following configuration
-  users.users.chrism = {
+  users.users.giezac = {
     isNormalUser = true;
     home = "/home/giezac";
     description = "Me";
