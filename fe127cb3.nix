1479004bb320771420b674d4567c9fad088cbe6e
Mr Z <denver.cfman@gmail.com>
2024-03-25 14:10:06 -0600

    Update fe127cb3.nix

--- fe127cb3.nix
+++ fe127cb3.nix
@@ -107,19 +107,19 @@
       echo "fedcba9876543211" > strings/0x409/serialnumber
       echo "TheWifiNinja" > strings/0x409/manufacturer
       echo "PI4 USB Device" > strings/0x409/product
-      mkdir -p /sys/kernel/config/usb_gadget/fe127cb3/configs/c.1/strings/0x409
+      ${pkgs.coreutils}/bin/mkdir -p /sys/kernel/config/usb_gadget/fe127cb3/configs/c.1/strings/0x409
       echo "Config 1: ECM network" > configs/c.1/strings/0x409/configuration
       echo 250 > configs/c.1/MaxPower
       # Add functions here
       # see gadget configurations below
       # End functions
-      mkdir -p /sys/kernel/config/usb_gadget/fe127cb3/functions/ecm.usb0
+      ${pkgs.coreutils}/bin/mkdir -p /sys/kernel/config/usb_gadget/fe127cb3/functions/ecm.usb0
       HOST="00:dc:c8:f7:75:14" # "HostPC"
       SELF="00:dd:dc:eb:6d:a1" # "BadUSB"
       echo $HOST > functions/ecm.usb0/host_addr
       echo $SELF > functions/ecm.usb0/dev_addr
       ln -s functions/ecm.usb0 configs/c.1/
-      udevadm settle -t 5 || :
+      ${pkgs.systemd}/bin/udevadm settle -t 5 || :
       ls /sys/class/udc > UDC
     '';
   };
